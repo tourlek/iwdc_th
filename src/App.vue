e2d253bc06fbbd66e1e6dc9bd15823964219514e
lek <stp.rodkeaw@gmaill.com>
2022-02-16 23:33:37 +0700

    setup custom.scss

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,14 @@
 </template>
 
 <style lang="scss">
+@import "./assets/style/custom.scss";
+@import "../node_modules/bootstrap-vue/src/index.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary;
 }
 
 #nav {
--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import "../scss/custom.scss";
-
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue), Vue.use(IconsPlugin);

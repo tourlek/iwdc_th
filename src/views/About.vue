0f90d673bb12387bce0e906de01016850593d8a6
lek <stp.rodkeaw@gmaill.com>
2022-02-16 22:40:09 +0700

    setup project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <b-button variant="success">
+      <strong class="text-lowercase primary">BUTTON</strong>
+    </b-button>
   </div>
 </template>
